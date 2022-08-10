5afd9af6c82773d118f4a7af87564e81f2ea1527
Frank Cwitkowitz <cwitkowitz@gmail.com>
2022-08-10 09:37:57 +0300

    Added some docs to wrappers collapsing data prior to evaluation.

--- scripts/evaluators.py
+++ scripts/evaluators.py
@@ -107,53 +107,116 @@ class TablatureNoteEvaluator(_StackedNoteEvaluator):
 
 class MultipitchEvaluator(_MultipitchEvaluator):
     """
-    TODO
+    Simple wrapper to support stacked multi pitch estimates and ground-truth.
     """
 
     def unpack(self, estimated, reference):
         """
-        TODO
+        Attempt to unpack and collapse stacked multi pitch data.
+
+        Parameters
+        ----------
+        estimated : dict
+          Dictionary containing stacked multi pitch estimate
+        reference : dict
+          Dictionary containing stacked multi pitch ground-truth
+
+        Returns
+        ----------
+        multi_pitch_est : ndarray (F x N)
+          Estimated multi pitch data
+          F - number of discrete pitches
+          N - number of frames
+        multi_pitch_ref : ndarray (F x N)
+          Ground-truth multi pitch data
+          Same dimensions as multi_pitch_est
         """
 
-        estimated, reference = super().unpack(estimated, reference)
+        # Call the parent function to unpack the stacked data
+        stacked_multi_pitch_est, stacked_multi_pitch_ref = super().unpack(estimated, reference)
 
-        estimated = tools.stacked_multi_pitch_to_multi_pitch(estimated)
-        reference = tools.stacked_multi_pitch_to_multi_pitch(reference)
+        # Collapse the stacked multi pitch arrays
+        multi_pitch_est = tools.stacked_multi_pitch_to_multi_pitch(stacked_multi_pitch_est)
+        multi_pitch_ref = tools.stacked_multi_pitch_to_multi_pitch(stacked_multi_pitch_ref)
 
-        return estimated, reference
+        return multi_pitch_est, multi_pitch_ref
 
 
 class PitchListEvaluator(_PitchListEvaluator):
     """
-    TODO
+    Simple wrapper to support stacked pitch list estimates and ground-truth.
     """
 
     def unpack(self, estimated, reference):
         """
-        TODO
+        Attempt to unpack and collapse stacked pitch list data.
+
+        Parameters
+        ----------
+        estimated : dict
+          Dictionary containing stacked pitch list estimate
+        reference : dict
+          Dictionary containing stacked pitch list ground-truth
+
+        Returns
+        ----------
+        pitch_list_est : tuple (times_est, _pitch_list_est)
+          Estimated pitch list data
+          _pitch_list_est : list of ndarray (T1 x [...])
+            Collection of MIDI pitches
+          times_est : ndarray (T1)
+            Time in seconds associated with each frame
+          (T1 - number of observations in estimates (frames))
+        pitch_list_ref : tuple (times_ref, _pitch_list_ref)
+          Ground-truth pitch list data
+          _pitch_list_ref : list of ndarray (T2 x [...])
+            Collection of MIDI pitches
+          times_ref : ndarray (T2)
+            Time in seconds associated with each frame
+          (T2 - number of observations in ground-truth (frames))
         """
 
-        estimated, reference = super().unpack(estimated, reference)
+        # Call the parent function to unpack the stacked data
+        stacked_pitch_list_est, stacked_pitch_list_ref = super().unpack(estimated, reference)
 
-        estimated = tools.stacked_pitch_list_to_pitch_list(estimated)
-        reference = tools.stacked_pitch_list_to_pitch_list(reference)
+        # Collapse the stacked pitch lists
+        pitch_list_est = tools.stacked_pitch_list_to_pitch_list(stacked_pitch_list_est)
+        pitch_list_ref = tools.stacked_pitch_list_to_pitch_list(stacked_pitch_list_ref)
 
-        return estimated, reference
+        return pitch_list_est, pitch_list_ref
 
 
 class NoteEvaluator(_NoteEvaluator):
     """
-    TODO
+    Simple wrapper to support stacked note estimates and ground-truth.
     """
 
     def unpack(self, estimated, reference):
         """
-        TODO
+        Attempt to unpack and collapse stacked note data.
+
+        Parameters
+        ----------
+        estimated : dict
+          Dictionary containing stacked notes estimate
+        reference : dict
+          Dictionary containing stacked notes ground-truth
+
+        Returns
+        ----------
+        notes_est : ndarray (K x 3)
+          Estimated note intervals and pitches by row
+          K - number of estimated notes
+        notes_ref : ndarray (L x 3)
+          Ground-truth note intervals and pitches by row
+          L - number of ground-truth notes
         """
 
-        estimated, reference = super().unpack(estimated, reference)
+        # Call the parent function to unpack the stacked data
+        stacked_notes_est, stacked_notes_ref = super().unpack(estimated, reference)
 
-        estimated = tools.notes_to_batched_notes(*tools.stacked_notes_to_notes(estimated))
-        reference = tools.notes_to_batched_notes(*tools.stacked_notes_to_notes(reference))
+        # Collapse the stacked notes and convert them to batched representations
+        notes_est = tools.notes_to_batched_notes(*tools.stacked_notes_to_notes(stacked_notes_est))
+        notes_ref = tools.notes_to_batched_notes(*tools.stacked_notes_to_notes(stacked_notes_ref))
 
-        return estimated, reference
+        return notes_est, notes_ref
