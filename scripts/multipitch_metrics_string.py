daeaf3e6866f37c8c3f56f08860116ac290c9a04
Frank Cwitkowitz <cwitkowitz@gmail.com>
2022-08-12 10:47:21 +0300

    Cleaned up and added documentation to code for string-level metrics.

--- scripts/multipitch_metrics_string.py
+++ scripts/multipitch_metrics_string.py
@@ -1,9 +1,5 @@
-'''
-TODO - if opening mir_eval pull request, update documentation in similar manner to
-       https://github.com/cwitkowitz/mir_eval/blob/master/mir_eval/transcription_velocity.py
-'''
+# Author: Frank Cwitkowitz <fcwitkow@ur.rochester.edu>
 
-import numpy as np
 from mir_eval.multipitch import validate, \
                                 resample_multipitch, \
                                 frequencies_to_midi, \
@@ -13,102 +9,125 @@ from mir_eval.multipitch import validate, \
                                 compute_accuracy, \
                                 compute_err_score
 from mir_eval import util
+
+import numpy as np
 import warnings
 
 
-def pre_validate(ref_time, ref_freqs, ref_sources,
-                 est_time, est_freqs, est_sources):
+def validate_sources(times_ref, sources_ref, times_est, sources_est):
     """
-    TODO - if opening mir_eval pull request, update documentation in similar manner to
-           https://github.com/cwitkowitz/mir_eval/blob/master/mir_eval/transcription_velocity.py
+    Determine if valid sources have been provided, given
+    corresponding observation times, for both reference and estimates.
+
+    Parameters
+    ----------
+    times_ref : ndarray (N)
+      Time for each reference observation
+    sources_ref : ndarray (N)
+      Source for each reference observation
+    times_est : ndarray (L)
+      Time for each estimate observation
+    sources_est : ndarray (L)
+      Source for each estimate observation
     """
-    # Check that sources have the same length as number of frames
-    if not ref_sources.shape[0] == ref_time.shape[0]:
-        raise ValueError('Reference sources must have the same length as '
-                         'pitch observations.')
-    if not est_sources.shape[0] == est_time.shape[0]:
-        raise ValueError('Estimated sources must have the same length as '
-                         'pitch observations.')
-    if len(np.setdiff1d(ref_sources, est_sources)):
-        warnings.warn('Some reference sources have no corresponding '
-                      'observations in estimates. This could cause '
-                      'unwanted behavior if reference and estimates '
-                      'have different sampling periods for other sources.')
-    if len(np.setdiff1d(est_sources, ref_sources)):
-        warnings.warn('Some estimate sources have no corresponding '
-                      'observations in reference. This could cause '
-                      'unwanted behavior if reference and estimates '
-                      'have different sampling periods.')
+
+    # Check that sources have length equal to the number of observations
+    if not sources_ref.shape[0] == times_ref.shape[0]:
+        raise ValueError('Reference sources must have the same length as pitch observations.')
+    if not sources_est.shape[0] == times_est.shape[0]:
+        raise ValueError('Estimated sources must have the same length as pitch observations.')
 
 
-def multipitch_metrics(ref_time, ref_freqs, ref_sources,
-                       est_time, est_freqs, est_sources, **kwargs):
+def multipitch_metrics(ref_time, ref_freqs, ref_sources, est_time, est_freqs, est_sources, **kwargs):
     """
-    TODO - if opening mir_eval pull request, update documentation in similar manner to
-           https://github.com/cwitkowitz/mir_eval/blob/master/mir_eval/transcription_velocity.py
+    Evaluate frame-wise continuous multipitch predictions
+    when taking into account the source of the pitches.
+
+    Parameters
+    ----------
+    ref_sources : ndarray (N)
+      Source for each reference observation
+    est_sources : ndarray (L)
+      Source for each estimate observation
+    See mir_eval.multipitch.metrics for others...
+
+    Returns
+    ----------
+    See mir_eval.multipitch.metrics...
     """
-    pre_validate(ref_time, ref_freqs, ref_sources,
-                 est_time, est_freqs, est_sources)
 
-    n_ref, n_est = np.empty(0), np.empty(0)
+    # Make sure the provided sources are valid
+    validate_sources(ref_time, ref_sources, est_time, est_sources)
 
-    true_positives, true_positives_chroma = np.empty(0), np.empty(0)
+    # Create empty arrays to hold counts
+    n_ref = np.empty(0)
+    n_est = np.empty(0)
+    true_positives = np.empty(0)
+    true_positives_chroma = np.empty(0)
 
-    for src in np.unique(np.concatenate((ref_sources, est_sources))):
-        src_ref_idcs = (ref_sources == src)
-        src_est_idcs = (est_sources == src)
+    # Determine which sources are present in the reference/estimate
+    all_sources = np.unique(np.concatenate((ref_sources, est_sources)))
 
+    for src in all_sources:
+        # Determine which indices in reference/estimate correspond to the source
+        src_ref_idcs, src_est_idcs = (ref_sources == src), (est_sources == src)
+
+        # Obtain the reference/estimate time arrays for the source
         _ref_time, _est_time = ref_time[src_ref_idcs], est_time[src_est_idcs]
 
+        # Obtain the reference/estimate pitch lists for the source
         _ref_freqs = [ref_freqs[i] for i in np.where(src_ref_idcs)[0]]
         _est_freqs = [est_freqs[i] for i in np.where(src_est_idcs)[0]]
 
+        # Make sure the pitch list data for this source is valid
         validate(_ref_time, _ref_freqs, _est_time, _est_freqs)
 
-        # resample est_freqs if est_times is different from ref_times
+        # Check if there are sources in estimates that are not present in reference
+        if not _ref_time.size:
+            raise ValueError(f'No reference data for source \'{src}\'. Resampling '
+                             f'will nullify estimates. Empty observations should '
+                             f'be provided as reference data for this source.')
+
+        # Resample estimated pitch list if estimate times differ from reference
         if _est_time.size != _ref_time.size or not np.allclose(_est_time, _ref_time):
-            warnings.warn("Estimate times not equal to reference times for source {}. "
-                          "Resampling to common time base.".format(src))
+            warnings.warn(f'Estimate times not equal to reference times for '
+                          f'source \'{src}\'. Resampling to common time base.')
             _est_freqs = resample_multipitch(_est_time, _est_freqs, _ref_time)
 
-        # convert frequencies from Hz to continuous midi note number
-        ref_freqs_midi = frequencies_to_midi(_ref_freqs)
-        est_freqs_midi = frequencies_to_midi(_est_freqs)
+        # Convert frequencies from Hz to continuous midi number
+        _ref_freqs_midi = frequencies_to_midi(_ref_freqs)
+        _est_freqs_midi = frequencies_to_midi(_est_freqs)
 
-        # count number of occurrences across
-        n_ref = np.append(n_ref, compute_num_freqs(ref_freqs_midi))
-        n_est = np.append(n_est, compute_num_freqs(est_freqs_midi))
+        # Count number of estimate and reference pitches for the source
+        n_ref = np.append(n_ref, compute_num_freqs(_ref_freqs_midi))
+        n_est = np.append(n_est, compute_num_freqs(_est_freqs_midi))
 
-        # compute chroma wrapped midi number
-        ref_freqs_chroma = midi_to_chroma(ref_freqs_midi)
-        est_freqs_chroma = midi_to_chroma(est_freqs_midi)
+        # Compute chroma wrapped continuous midi number
+        _ref_freqs_chroma = midi_to_chroma(_ref_freqs_midi)
+        _est_freqs_chroma = midi_to_chroma(_est_freqs_midi)
 
-        # compute the number of true positives
+        # Compute the number of true positives for the source
         true_positives = np.append(true_positives, util.filter_kwargs(
-            compute_num_true_positives, ref_freqs_midi, est_freqs_midi, **kwargs))
+            compute_num_true_positives, _ref_freqs_midi, _est_freqs_midi, **kwargs))
 
-        # compute the number of true positives ignoring octave mistakes
-        true_positives_chroma = np.append(
-            true_positives_chroma, util.filter_kwargs(
-            compute_num_true_positives, ref_freqs_chroma,
-            est_freqs_chroma, chroma=True, **kwargs))
+        # Compute the number of true positives for the source while ignoring octave mistakes
+        true_positives_chroma = np.append(true_positives_chroma,
+                                          util.filter_kwargs(compute_num_true_positives, _ref_freqs_chroma,
+                                                             _est_freqs_chroma, chroma=True, **kwargs))
 
-    # compute accuracy metrics
-    precision, recall, accuracy = compute_accuracy(
-        true_positives, n_ref, n_est)
+    # Compute accuracy metrics across all sources
+    precision, recall, accuracy = compute_accuracy(true_positives, n_ref, n_est)
 
-    # compute error metrics
-    e_sub, e_miss, e_fa, e_tot = compute_err_score(
-        true_positives, n_ref, n_est)
+    # Compute error metrics across all sources
+    e_sub, e_miss, e_fa, e_tot = compute_err_score(true_positives, n_ref, n_est)
 
-    # compute accuracy metrics ignoring octave mistakes
-    precision_chroma, recall_chroma, accuracy_chroma = compute_accuracy(
-        true_positives_chroma, n_ref, n_est)
+    # Compute accuracy metrics across all sources while ignoring octave mistakes
+    precision_chroma, recall_chroma, accuracy_chroma = \
+        compute_accuracy(true_positives_chroma, n_ref, n_est)
 
-    # compute error metrics ignoring octave mistakes
-    e_sub_chroma, e_miss_chroma, e_fa_chroma, e_tot_chroma = compute_err_score(
-        true_positives_chroma, n_ref, n_est)
+    # Compute error metrics across all sources while ignoring octave mistakes
+    e_sub_chroma, e_miss_chroma, e_fa_chroma, e_tot_chroma = \
+        compute_err_score(true_positives_chroma, n_ref, n_est)
 
-    return (precision, recall, accuracy, e_sub, e_miss, e_fa, e_tot,
-            precision_chroma, recall_chroma, accuracy_chroma, e_sub_chroma,
-            e_miss_chroma, e_fa_chroma, e_tot_chroma)
+    return precision, recall, accuracy, e_sub, e_miss, e_fa, e_tot, precision_chroma, \
+           recall_chroma, accuracy_chroma, e_sub_chroma, e_miss_chroma, e_fa_chroma, e_tot_chroma
--- scripts/note_metrics_string.py
+++ scripts/note_metrics_string.py
@@ -1,47 +1,63 @@
-"""
-TODO - if opening mir_eval pull request, update documentation in similar manner to
-       https://github.com/cwitkowitz/mir_eval/blob/master/mir_eval/transcription_velocity.py
-"""
+# Author: Frank Cwitkowitz <fcwitkow@ur.rochester.edu>
 
-import numpy as np
 from mir_eval import transcription
 from mir_eval import util
 
-
-def validate(ref_intervals, ref_pitches, ref_sources, est_intervals,
-             est_pitches, est_sources):
-    """
-    TODO - if opening mir_eval pull request, update documentation in similar manner to
-           https://github.com/cwitkowitz/mir_eval/blob/master/mir_eval/transcription_velocity.py
-    """
-    transcription.validate(ref_intervals, ref_pitches, est_intervals,
-                           est_pitches)
-    # Check that sources have the same length as intervals/pitches
-    if not ref_sources.shape[0] == ref_pitches.shape[0]:
-        raise ValueError('Reference sources must have the same length as '
-                         'pitches and intervals.')
-    if not est_sources.shape[0] == est_pitches.shape[0]:
-        raise ValueError('Estimated sources must have the same length as '
-                         'pitches and intervals.')
+import numpy as np
 
 
-def match_notes(
-        ref_intervals, ref_pitches, ref_sources, est_intervals, est_pitches,
-        est_sources, onset_tolerance=0.05, pitch_tolerance=50.0,
-        offset_ratio=0.2, offset_min_tolerance=0.05, strict=False):
+def validate(intervals_ref, pitches_ref, sources_ref, intervals_est, pitches_est, sources_est):
     """
-    TODO - if opening mir_eval pull request, update documentation in similar manner to
-           https://github.com/cwitkowitz/mir_eval/blob/master/mir_eval/transcription_velocity.py
+    Determine if valid note groups have been provided for both reference and estimates.
+
+    Parameters
+    ----------
+    sources_ref : ndarray (n)
+      Source for each reference note
+    sources_est : ndarray (m)
+      Source for each estimated note
+    See mir_eval.transcription.validate for others...
     """
 
-    matching = []
+    # Check that sources have length equal to the number of pitches
+    if not sources_ref.shape[0] == pitches_ref.shape[0]:
+        raise ValueError('Reference sources and pitches have different lengths.')
+    if not sources_est.shape[0] == pitches_est.shape[0]:
+        raise ValueError('Estimated sources and pitches have different lengths.')
+
+    # Perform standard note validation steps
+    transcription.validate(intervals_ref, pitches_ref, intervals_est, pitches_est)
+
+
+def match_notes(ref_intervals, ref_pitches, ref_sources, est_intervals, est_pitches, est_sources,
+                onset_tolerance=0.05, pitch_tolerance=50.0, offset_ratio=0.2, offset_min_tolerance=0.05,
+                strict=False):
+    """
+    Compute the optimal matching between two sets of notes when taking into account the source of the notes.
+
+    Parameters
+    ----------
+    ref_sources : ndarray (n)
+      Source for each reference note
+    est_sources : ndarray (m)
+      Source for each estimated note
+    See mir_eval.transcription.match_notes for others...
+
+    Returns
+    ----------
+    See mir_eval.transcription.match_notes...
+    """
+
+    # Create a list to hold (reference note, estimate note) matches
+    matching = list()
 
     # Obtain a collection of global indices for estimated and reference notes
     glb_ref_idcs, glb_est_idcs = np.arange(len(ref_pitches)), np.arange(len(est_pitches))
 
+    # Loop through all unique sources in the reference data
     for src in np.unique(ref_sources):
-        src_ref_idcs = (ref_sources == src)
-        src_est_idcs = (est_sources == src)
+        # Determine which indices in reference/estimate correspond to the source
+        src_ref_idcs, src_est_idcs = (ref_sources == src), (est_sources == src)
 
         # Within scope of the source, compute note matching as usual
         src_matching = transcription.match_notes(
@@ -57,30 +73,52 @@ def match_notes(
     return matching
 
 
-def precision_recall_f1_overlap(
-        ref_intervals, ref_pitches, ref_sources, est_intervals, est_pitches,
-        est_sources, onset_tolerance=0.05, pitch_tolerance=50.0,
-        offset_ratio=0.2, offset_min_tolerance=0.05, strict=False,
-        beta=1.0):
+def precision_recall_f1_overlap(ref_intervals, ref_pitches, ref_sources, est_intervals, est_pitches,
+                                est_sources, onset_tolerance=0.05, pitch_tolerance=50.0, offset_ratio=0.2,
+                                offset_min_tolerance=0.05, strict=False, beta=1.0):
     """
-    TODO - if opening mir_eval pull request, update documentation in similar manner to
-           https://github.com/cwitkowitz/mir_eval/blob/master/mir_eval/transcription_velocity.py
+    Evaluate intervallic note predictions when taking into account the source of the notes.
+
+    Parameters
+    ----------
+    ref_sources : ndarray (n)
+      Source for each reference note
+    est_sources : ndarray (m)
+      Source for each estimated note
+    See mir_eval.transcription.precision_recall_f1_overlap for others...
+
+    Returns
+    ----------
+    See mir_eval.transcription.precision_recall_f1_overlap...
     """
+
+    # Make sure the provided reference and estimated notes are valid
     validate(ref_intervals, ref_pitches, ref_sources, est_intervals, est_pitches, est_sources)
-    # When reference notes are empty, metrics are undefined, return 0's
-    if len(ref_pitches) == 0 or len(est_pitches) == 0:
+
+    # Count number of reference and estimated notes
+    n_ref = len(ref_pitches)
+    n_est = len(est_pitches)
+
+    # When reference notes are empty,
+    # metrics are undefined, return 0's
+    if n_ref == 0 or n_est == 0:
         return 0., 0., 0., 0.
 
-    matching = match_notes(
-        ref_intervals, ref_pitches, ref_sources, est_intervals, est_pitches,
-        est_sources, onset_tolerance, pitch_tolerance, offset_ratio,
-        offset_min_tolerance, strict)
+    # Obtain a list of (reference note, estimate note) match estimates
+    matching = match_notes(ref_intervals, ref_pitches, ref_sources,
+                           est_intervals, est_pitches, est_sources,
+                           onset_tolerance, pitch_tolerance, offset_ratio,
+                           offset_min_tolerance, strict)
 
-    precision = float(len(matching))/len(est_pitches)
-    recall = float(len(matching))/len(ref_pitches)
+    # Count number of true positive estimates
+    true_positives = len(matching)
+
+    # Compute precision, recall, and f1-score
+    precision = float(true_positives) / n_est
+    recall = float(true_positives) / n_ref
     f_measure = util.f_measure(precision, recall, beta=beta)
 
-    avg_overlap_ratio = transcription.average_overlap_ratio(
-        ref_intervals, est_intervals, matching)
+    # Compute the overlap ration between all matched notes
+    avg_overlap_ratio = transcription.average_overlap_ratio(ref_intervals, est_intervals, matching)
 
     return precision, recall, f_measure, avg_overlap_ratio
